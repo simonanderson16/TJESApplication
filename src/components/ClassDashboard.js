4ad2d3fb35700d352ee3569151f14fe7b685d348
simonanderson16 <uhx8bu@virginia.edu>
2023-05-23 17:01:13 -0400

    started implementing search feature

--- src/components/ClassDashboard.js
+++ src/components/ClassDashboard.js
@@ -1,12 +1,24 @@
 
 import ClassDashboardRow from "./ClassDashboardRow";
 import "../classDashboardStyles.css"
+import { useReducer, useRef, useState } from "react";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function ClassDashboard({classCollection, gradeCollection, userCollection}) {
-    console.log("dash,class", classCollection)
-    console.log("dash,grade", gradeCollection)
-    console.log("dash,user", userCollection)
-    
+    // console.log("dash,class", classCollection)
+    // console.log("dash,grade", gradeCollection)
+    // console.log("dash,user", userCollection)
+    console.log(classCollection)
+
+    const textInputRef = useRef(null);
+
+    const performSearch = () => {
+        const inputValue = textInputRef.current.value;
+        const filteredValues = Object.values(classCollection).filter((classItem) => classItem.name === inputValue);
+        console.log(filteredValues);
+    }
+
     let userType = 'admin';
 
     return (
@@ -16,17 +28,16 @@ export default function ClassDashboard({classCollection, gradeCollection, userCo
             <> 
             <button>Add Class</button>
             <div className="admin-class-search">
-                <input type="text" placeholder="Search Class by Name"/>
-                <button>Search</button>
+                <input type="text" placeholder="Search Class by Name" ref={textInputRef}/>
+                <button onClick={performSearch}>Search</button>
             </div>
             </>
             : null}
-            <ClassDashboardRow/>
-            <ClassDashboardRow/>
-            <ClassDashboardRow/>
-            <ClassDashboardRow/>
-            <ClassDashboardRow/>
-
+            {Object.keys(classCollection).map((item,index)=>
+                <div key = {index}>
+                    <ClassDashboardRow name={classCollection[item].name} teacher={classCollection[item].teacher}/>
+                </div>
+            )}
         </div>
     )
 }
--- src/components/ClassDashboardRow.js
+++ src/components/ClassDashboardRow.js
@@ -1,12 +1,25 @@
 import "../classDashboardStyles.css"
+import { getDocs, doc, getDoc } from "firebase/firestore"
+import { db } from "../firebase";
+import { useState } from "react";
 
 
-export default function ClassDashboardRow() {
+export default function ClassDashboardRow({name, teacher}) {
+
+    const teacherID = teacher.id;
+    const [teacherName, setTeacherName] = useState();
+    
+
+    getDoc(doc(db, "User", teacherID))
+        .then((doc) => setTeacherName(doc.data().firstName + " " + doc.data().lastName))
+
+
+    console.log("teacher: ", teacher.id)
+
     return (
         <div className="class-dashboard-row">
-            <p className="class-name"> Class Name </p>
-            <p className="teacher-name"> Teacher </p>
-            <p className="row-grade"> Grade </p>
+            <p className="class-name">Class Name: {name}</p>
+            <p className="teacher-name">Teacher: {teacherName}</p>
         </div>
     )
 }
