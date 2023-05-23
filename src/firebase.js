ea5d4788804990cc5e70848936963b9333da96cd
simonanderson16 <uhx8bu@virginia.edu>
2023-05-23 13:17:07 -0400

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_M9rE4-1enByfMMyesYiql9r2p_7AyVU",
+  authDomain: "tjes-application.firebaseapp.com",
+  projectId: "tjes-application",
+  storageBucket: "tjes-application.appspot.com",
+  messagingSenderId: "978271925744",
+  appId: "1:978271925744:web:226661e61cb0b0e8b0d935"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {db}
