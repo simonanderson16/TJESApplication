01f3fc47416cbad629743a7ac1a4ca4a933f2dbc
simonanderson16 <uhx8bu@virginia.edu>
2023-05-23 08:37:47 -0400

    changed navigation buttons

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../HomePageStyles.css"
 
 export default function HomePage() {
+
+    const navigate = useNavigate();
+
+    const goToClassDashboard = () => {
+        navigate("/classes");
+    }
+
+    const goToStudentDirectory = () => {
+        navigate("/students");
+    }
+
+    const goToTeacherDirectory = () => {
+        navigate("/teachers");
+    }
+
+    const goToCalendar = () => {
+        navigate("/calendar");
+    }
+
     return (
         <div className="homepage-container">
             <h1>Home</h1>
             <div className="homepage-grid">
-                <div className="homepage-box">
+                {/* <div className="homepage-box">
                     <Link to="classes">Class Dashboard</Link>
-                </div>
-                <div className="homepage-box">
+                </div> */}
+                <button className="homepage-box" onClick={goToClassDashboard}>Class Dashboard</button>
+                <button className="homepage-box" onClick={goToStudentDirectory}>Student Directory</button>
+                <button className="homepage-box" onClick={goToTeacherDirectory}>Teacher Directory</button>
+                <button className="homepage-box" onClick={goToCalendar}>Calendar</button>
+                {/* <div className="homepage-box">
                     <Link to="students">Student Directory</Link>
                 </div>
                 <div className="homepage-box">
@@ -17,7 +40,7 @@ export default function HomePage() {
                 </div>
                 <div className="homepage-box">
                     <Link to="calendar">Calendar</Link>
-                </div>
+                </div> */}
             </div>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
 import "../NavbarStyles.css"
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+
+    const navigate = useNavigate();
+
+    const goHome = () => {
+        navigate("/");
+    }
+
     return (
         <div className="navbar">
             <img src={require("./TJESLogo.png")} className="tjes-logo"></img>
-            <Link to="/" className="router-link">Home</Link>
+            {/* <Link to="/" className="router-link">Home</Link> */}
+            <button onClick={goHome}>Home</button>
+
         </div>
     )
 }
