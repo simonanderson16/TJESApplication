3b76c688537934cd233c4f98cc1d60ec3ff18bdf
RichyKim12 <rmk9ds@virginia.edu>
2023-05-23 16:14:51 -0400

    Update StudentDirectory.js

--- src/components/StudentDirectory.js
+++ src/components/StudentDirectory.js
@@ -3,6 +3,7 @@ import Student from "./Student";
 import StudentCollectionList from "./StudentCollectionList";
 export default function StudentDirectory({userCollection}) {
     //TODO: Add search bar that searches students by email
+    // TODO: Make "add student" which is only viewable by an admin
     return (
         <div className="student-directory-container">
             <p>Student Directory</p>
