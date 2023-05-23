6e35d01fe29f078bd4f40586f390b028acd6da0e
simonanderson16 <uhx8bu@virginia.edu>
2023-05-23 12:28:14 -0400

    changed home button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import "../NavbarStyles.css"
 import { Link, useNavigate } from "react-router-dom";
+import {FaHome} from "react-icons/fa";
 
 export default function Navbar() {
 
@@ -13,7 +14,8 @@ export default function Navbar() {
         <div className="navbar">
             <img src={require("./TJESLogo.png")} className="tjes-logo"></img>
             {/* <Link to="/" className="router-link">Home</Link> */}
-            <button onClick={goHome}>Home</button>
+            {/* <button onClick={goHome}><FaHome/></button> */}
+            <FaHome className="home-button" onClick={goHome}/>
 
         </div>
     )
