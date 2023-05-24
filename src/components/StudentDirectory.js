52b62f9678c6e3d731c4e1c93cd00c705cf53837
William Kieffer <william.kieffer@outlook.com>
2023-05-24 12:28:25 -0400

    Add create student user

--- src/components/StudentDirectory.js
+++ src/components/StudentDirectory.js
@@ -1,70 +1,78 @@
-
+import UserBuilder from "./UserBuilder";
 import StudentCollectionList from "./StudentCollectionList";
-import {useState, useEffect} from 'react'
-import { getFirestore, getDocs, collection, where, query } from "firebase/firestore"; 
-import {db} from "../firebase.js"
+import { useState, useEffect } from 'react'
+import { getFirestore, getDocs, collection, where, query } from "firebase/firestore";
+import { db } from "../firebase.js";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-export default function StudentDirectory({userCollection}) {
+export default function StudentDirectory({ userCollection }) {
     //TODO: Add search bar that searches students by email
     // TODO: Make "add student" which is only viewable by an admin
     const [searchString, setSearchString] = useState('');
     const [filterSearch, setFilterSearch] = useState(false);
-    const [filteredCollection, setFilteredCollection] = useState([])
+    const [filteredCollection, setFilteredCollection] = useState([]);
+    const [buildingUser, setBuildingUser] = useState(false);
 
     //function where if user hits enter on textarea, 
     const handleSearch = async (event) => {
-        setSearchString(event.target.value)
-        
+        setSearchString(event.target.value);
+
     }
-    
-      const handleEnteredSearch = async (event) => {
-        if (event.key === "Enter"){
+
+    const handleEnteredSearch = async (event) => {
+        if (event.key === "Enter") {
             // Prevent page refresh
             // SRC: https://stackoverflow.com/questions/50193227/basic-react-form-submit-refreshes-entire-page
-            event.preventDefault(); 
-            console.log("string", searchString)
-            const q = query(collection(db, "User"), where("userType", "==", "student" ), where("firstName", "<=", searchString));
+            event.preventDefault();
+            console.log("string", searchString);
+            const q = query(collection(db, "User"), where("userType", "==", "student"), where("firstName", "<=", searchString));
             let data = []
             const querySnapshot = await getDocs(q);
             querySnapshot.forEach((doc) => {
                 data.push(doc.data())
             });
-            console.log("Data",data)
+            console.log("Data", data)
             setFilteredCollection(data)
-            if (searchString.trim() === ''){ // if they hit enter on empty field
+            if (searchString.trim() === '') { // if they hit enter on empty field
                 setFilterSearch(false)
                 event.preventDefault();
             }
-            else{ // if they hit enter on non-empty field
+            else { // if they hit enter on non-empty field
                 setFilterSearch(true)
                 event.preventDefault();
             }
         }
-      }
+    }
 
 
     return (
         <div className="student-directory-container">
             <h1>Student Directory</h1>
-            <div className = "Search">
+            <button onClick={() => setBuildingUser(true)}>Add Student</button>
+            <div hidden={!buildingUser}>
+                <UserBuilder
+                    userType={'student'}
+                ></UserBuilder>
+            </div>
+            <div className="Search">
                 <form >
                     <label> Search by name </label>
                     <input
-                    type = "text"
-                    required
-                    value = {searchString}
-                    onChange={handleSearch}
-                    onKeyDown = {handleEnteredSearch}
+                        type="text"
+                        required
+                        value={searchString}
+                        onChange={handleSearch}
+                        onKeyDown={handleEnteredSearch}
                     />
-                </form>  
+                </form>
                 {searchString}
             </div>
 
-            {filterSearch === true? 
-            (<><StudentCollectionList studentCollection={filteredCollection}/></>):
-            (<><StudentCollectionList studentCollection={userCollection.filter((user) => user.userType==='student')}/></>)}
+            {filterSearch === true ?
+                (<><StudentCollectionList studentCollection={filteredCollection} /></>) :
+                (<><StudentCollectionList studentCollection={userCollection.filter((user) => user.userType === 'student')} /></>)}
 
-            
+
         </div>
     )
 }
