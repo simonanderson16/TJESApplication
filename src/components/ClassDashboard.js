38a6b8e0216e67051b22f62b6146a52a30947d94
simonanderson16 <uhx8bu@virginia.edu>
2023-05-23 20:00:34 -0400

    implemented search for class dashboard

--- src/components/ClassDashboard.js
+++ src/components/ClassDashboard.js
@@ -6,6 +6,34 @@ import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 
 export default function ClassDashboard({classCollection, gradeCollection, userCollection}) {
+
+    let userType = 'admin';
+
+    const [searchString, setSearchString] = useState('');
+    const [filterSearch, setFilterSearch] = useState(false);
+
+    //function where if user hits enter on textarea, 
+    const handleSearch = (event) => {
+        setSearchString(event.target.value)
+    }
+    
+      const handleEnteredSearch = (event) => {
+        if (event.key === "Enter"){
+            // Prevent page refresh
+            // SRC: https://stackoverflow.com/questions/50193227/basic-react-form-submit-refreshes-entire-page
+            if (searchString.trim() === ''){ // if they hit enter on empty field
+                setFilterSearch(false)
+                event.preventDefault();
+            }
+            else{ // if they hit enter on non-empty field
+                setFilterSearch(true)
+                event.preventDefault();
+            }
+            event.preventDefault(); 
+        }
+        return false
+      }
+
     // console.log("dash,class", classCollection)
     // console.log("dash,grade", gradeCollection)
     // console.log("dash,user", userCollection)
@@ -19,8 +47,6 @@ export default function ClassDashboard({classCollection, gradeCollection, userCo
         console.log(filteredValues);
     }
 
-    let userType = 'admin';
-
     return (
         <div className="class-dashboard-container">
             <h1>{userType==='admin' ? "All Classes" : "My Classes"}</h1>
@@ -28,16 +54,38 @@ export default function ClassDashboard({classCollection, gradeCollection, userCo
             <> 
             <button>Add Class</button>
             <div className="admin-class-search">
-                <input type="text" placeholder="Search Class by Name" ref={textInputRef}/>
-                <button onClick={performSearch}>Search</button>
+                {/* <input type="text" placeholder="Search Class by Name" ref={textInputRef}/>
+                <button onClick={performSearch}>Search</button> */}
+                <form >
+                    <label> Search by class name </label>
+                    <input
+                    type = "text"
+                    required
+                    value = {searchString}
+                    onChange={handleSearch}
+                    onKeyDown = {handleEnteredSearch}
+                    />
+                </form>  
             </div>
             </>
             : null}
-            {Object.keys(classCollection).map((item,index)=>
+            {filterSearch === true ? 
+            (Object.keys((classCollection).filter((classItem) => classItem.name === searchString)).map((item,index)=>
                 <div key = {index}>
                     <ClassDashboardRow name={classCollection[item].name} teacher={classCollection[item].teacher}/>
                 </div>
-            )}
+            )) : 
+            (Object.keys(classCollection).map((item,index)=>
+            <div key = {index}>
+                <ClassDashboardRow name={classCollection[item].name} teacher={classCollection[item].teacher}/>
+            </div>
+        ))
+            }
+            {/* {Object.keys(classCollection).map((item,index)=>
+                <div key = {index}>
+                    <ClassDashboardRow name={classCollection[item].name} teacher={classCollection[item].teacher}/>
+                </div>
+            )} */}
         </div>
     )
 }
