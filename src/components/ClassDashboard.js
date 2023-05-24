59390e6f64454c7bf4a1daa60f1ad71c330e28d4
simonanderson16 <uhx8bu@virginia.edu>
2023-05-24 11:21:40 -0400

    implemented creating new class

--- src/components/ClassDashboard.js
+++ src/components/ClassDashboard.js
@@ -2,15 +2,59 @@
 import ClassDashboardRow from "./ClassDashboardRow";
 import "../classDashboardStyles.css"
 import { useReducer, useRef, useState } from "react";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, addDoc, doc } from "firebase/firestore";
 import { db } from "../firebase";
 
 export default function ClassDashboard({classCollection, gradeCollection, userCollection}) {
 
     let userType = 'admin';
 
+    const teacherCollection = userCollection.filter((user) => user.userType === 'teacher');
+
     const [searchString, setSearchString] = useState('');
     const [filterSearch, setFilterSearch] = useState(false);
+    const [addingClass, setAddingClass] = useState(false);
+    const [currentTeacherSelection, setCurrentTeacherSelection] = useState();
+    const [addClassName, setAddClassName] = useState();
+
+
+    const handleAddClassButton = (event) => {
+        setAddingClass(true);
+      };
+
+      const handleSelectChange = (event) => {
+        setCurrentTeacherSelection(event.target.value);
+      };
+
+      const handleSetClassName = (event) => {
+        setAddClassName(event.target.value)
+      }
+
+      const addClass = async (className, teacherId) => {
+        const teacherRef = doc(db, "User", teacherId);
+      
+        const classRef = await addDoc(collection(db, "Class"), {
+          name: className,
+          teacher: teacherRef,
+        });
+      
+        // Add a subcollection "Grades" to the newly created class document
+        await addDoc(collection(classRef, "Grades"), {
+            // empty for now
+        });
+      };
+
+      const handleSubmitButton = (event) => {
+        setAddingClass(false);
+        if(currentTeacherSelection === 'Select Teacher') {
+            console.log("No selection")
+            return;
+        } else {
+            console.log(currentTeacherSelection);
+            addClass(addClassName, currentTeacherSelection);
+        }
+      };
+
 
     //function where if user hits enter on textarea, 
     const handleSearch = (event) => {
@@ -37,7 +81,6 @@ export default function ClassDashboard({classCollection, gradeCollection, userCo
     // console.log("dash,class", classCollection)
     // console.log("dash,grade", gradeCollection)
     // console.log("dash,user", userCollection)
-    console.log(classCollection)
 
     const textInputRef = useRef(null);
 
@@ -52,7 +95,23 @@ export default function ClassDashboard({classCollection, gradeCollection, userCo
             <h1>{userType==='admin' ? "All Classes" : "My Classes"}</h1>
             {userType==='admin' ?
             <> 
-            <button>Add Class</button>
+            <button onClick={handleAddClassButton}>Add Class</button>
+            </>
+            : null}
+            {addingClass ? 
+            (<>
+                <h3>Add Class</h3>
+                <p>Class Name:</p>
+                <input type="text" placeholder="Class Name" onChange={handleSetClassName}></input>
+                <p>Teacher:</p>
+                <select value={currentTeacherSelection} onChange={handleSelectChange}>
+                    <option value={null}>Select Teacher</option>
+                    {teacherCollection.map((teacher, index) => {return <option key={index} value={teacher.id}>{teacher.firstName + " " + teacher.lastName}</option>})}
+                </select>
+                <button onClick={handleSubmitButton}>submit</button>
+            </>)
+            :
+            (null)}
             <div className="admin-class-search">
                 {/* <input type="text" placeholder="Search Class by Name" ref={textInputRef}/>
                 <button onClick={performSearch}>Search</button> */}
@@ -67,8 +126,6 @@ export default function ClassDashboard({classCollection, gradeCollection, userCo
                     />
                 </form>  
             </div>
-            </>
-            : null}
             {filterSearch === true ? 
             (Object.keys((classCollection).filter((classItem) => classItem.name === searchString)).map((item,index)=>
                 <div key = {index}>
