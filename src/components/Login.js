cfe82d22a42c9e4443f5b757e7a49db0e4631dc0
simonanderson16 <uhx8bu@virginia.edu>
2023-05-25 14:13:03 -0400

    login page css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import './login.css'
 
 
 function Login() {
@@ -14,21 +15,23 @@ function Login() {
             navigate("/home");
         } catch (error) {
             const errorCode = error.code;
-            const errorMessage = error.message;
+            const errorMessage = "Incorrect Username or Password";
             document.getElementById('login-error-message').innerHTML = errorMessage;
         }
     }
 
     return (
         <div>
-            <h1>TJES SIS Sign In</h1>
-            <input className="login-input" placeholder="Email" id="login-input-email" />
-            <div><input className="login-input" type="password" placeholder="Password" id="login-input-password" /></div>
-            <button className="login-input" onClick={() => {
-                const email = document.getElementById('login-input-email').value;
-                const password = document.getElementById('login-input-password').value;
-                signin(email, password);
-            }}>Sign In</button>
+            <h1>TJES Dashboard Login</h1>
+            <div className="login-container">
+                <input className="login-input" placeholder="Email" id="login-input-email" />
+                <div><input className="login-input" type="password" placeholder="Password" id="login-input-password" /></div>
+                <button className="login-button" onClick={() => {
+                    const email = document.getElementById('login-input-email').value;
+                    const password = document.getElementById('login-input-password').value;
+                    signin(email, password);
+                }}>Sign In</button>
+            </div>
             <p id="login-error-message"></p>
         </div>
     );
