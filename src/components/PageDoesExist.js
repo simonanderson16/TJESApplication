cc08c9828482031f19d8f5fe9bc5b30fb94337db
RichyKim12 <rmk9ds@virginia.edu>
2023-05-25 09:59:17 -0400

    Added page does not exist page

---
+++ src/components/PageDoesExist.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function PageDoesExist() {
+  return (
+    <>    
+    <h1>Error: PageDoesExist</h1>
+    <h2>Enter a valid URL and try again</h2>
+    </>
+
+  )
+}
+
+export default PageDoesExist
